f1b0968aac8076deecdff81873985ebd5a6e42c2
ana.sdp <107457070+Septianadwi@users.noreply.github.com>
2023-08-11 22:18:29 +0700

    Create file Xcode
    Menambahkan file Xcode dengan nama Little Lemon.

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Mac Pro on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
